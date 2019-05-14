b0e5a6657ceff3e758c5c071f4fa7f52f1d3a430
Ian Yang <iany.yang@mail.utoronto.ca>
2019-05-13 20:14:49 -0400

    Updates

---
+++ precache-manifest.0756ed1ccc13769c633b900bdb7b3c10.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61bf51f2b88df38687aa0e65f896057c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "2729be7f57ade18f87ee",
+    "url": "/static/js/main.86d3958f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.0756ed1ccc13769c633b900bdb7b3c10.js"
 );
 
 self.addEventListener('message', (event) => {
