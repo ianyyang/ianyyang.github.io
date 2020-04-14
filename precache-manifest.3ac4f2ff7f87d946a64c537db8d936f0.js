d76b2fae25e43b205048041d0dd33372e742ead2
Ian Yang <iany.yang@mail.utoronto.ca>
2020-04-14 00:42:35 -0400

    Updates

---
+++ precache-manifest.3ac4f2ff7f87d946a64c537db8d936f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f09497e19c84680c64a9319b0ff381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f63e169cbe89cba8ad1",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "a776c66b35c09019c575",
+    "url": "/static/css/main.8f437af0.chunk.css"
+  },
+  {
+    "revision": "0f63e169cbe89cba8ad1",
+    "url": "/static/js/2.9b400012.chunk.js"
+  },
+  {
+    "revision": "a776c66b35c09019c575",
+    "url": "/static/js/main.87257a52.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
